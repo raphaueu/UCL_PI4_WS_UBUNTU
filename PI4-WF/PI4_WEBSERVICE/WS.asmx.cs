8f2ee5226df812d40965865e14300abb3d5f89df
Raphael Bezerra <rphlmbzr@gmail.com>
2017-06-05 20:13:14 -0300

    correção

--- PI4-WF/PI4_WEBSERVICE/WS.asmx.cs
+++ PI4-WF/PI4_WEBSERVICE/WS.asmx.cs
@@ -8,21 +8,18 @@ using System.Xml.Serialization;
 
 namespace PI4_WEBSERVICE
 {
-    /// <summary>
-    /// Descrição resumida de WS
-    /// </summary>
+    
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
-    // Para permitir que esse serviço da web seja chamado a partir do script, usando ASP.NET AJAX, remova os comentários da linha a seguir. 
-    // [System.Web.Script.Services.ScriptService]
+    
     public class WS : System.Web.Services.WebService
     {
         private RepositorioSala repsala;
 
-        [XmlInclude(typeof(sala))]
-        [XmlInclude(typeof(funcionario))]
-        [XmlInclude(typeof(predio))]
+      //  [XmlInclude(typeof(sala))]
+      //  [XmlInclude(typeof(funcionario))]
+      //  [XmlInclude(typeof(predio))]
 
         [WebMethod]
           
